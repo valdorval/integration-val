2459006ea15c79c97fa289d0832393dd8b140a02
val <vale.dorval@gmail.com>
2020-10-14 14:42:45 -0400

    accueil

--- template-parts/accueil.php
+++ template-parts/accueil.php
@@ -1,20 +1,51 @@
 <main class="main">
-     <h3 class="heading-tertiary">Suivez vos clients du bout des doigts</h3>
-     <section class="acceuil">
-          <div class="flex">
-               <div>
-                    <img src="" alt="icone1">
-                    <p>Grâce à notre nouveau CRM Wordpress, vous pourrez facilement faire le suivi de vos intéractions avec vos clients</p>
+     <h3 class="heading-tertiary u-padding-bottom-big">Suivez vos clients du bout des doigts</h3>
+     <section class="accueil container">
+          <p class="center u-padding-bottom-big">Grâce à notre nouveau CRM Wordpress, vous pourrez facilement faire le suivi de vos intéractions avec vos clients</p>
+
+          <div class="accueil__card flex">
+               <div class="accueil__card--img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/008-folder.png" alt="icone1">
                </div>
-               <div>
-                    <img src="" alt="icone1">
-                    <p></p>
-               </div>
-               <div>
-                    <img src="" alt="icone1">
-                    <p></p>
-               </div>
+               <p class="accueil__card--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus voluptate quasi quo sapiente? Asperiores, est voluptatibus delectus quidem illum praesentium, quia temporibus magnam, iusto fuga esse aperiam quae tempora iste.</p>
           </div>
+          <div class="accueil__card flex">
+               <div class="accueil__card--img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/006-bar-graph.png" alt="icone2">
+               </div>
+
+               <p class="accueil__card--text">blabla</p>
+          </div>
+          <div class="accueil__card flex">
+               <div class="accueil__card--img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/007-file.png" alt="icone2">
+               </div>
+
+               <p class="accueil__card--text">blabla</p>
+          </div>
+          <div class="accueil__card flex">
+               <div class="accueil__card--img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/004-analysis.png" alt="icone2">
+               </div>
+
+               <p class="accueil__card--text">blabla</p>
+          </div>
+          <div class="accueil__card flex">
+               <div class="accueil__card--img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/010-team.png" alt="icone2">
+               </div>
+
+               <p class="accueil__card--text">blabla</p>
+          </div>
+          <div class="accueil__card flex">
+               <div class="accueil__card--img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/009-notebook.png" alt="icone2">
+               </div>
+
+               <p class="accueil__card--text">blabla</p>
+          </div>
+
+
 
      </section>
 </main>
