a0bdf809f6dc2fe406b6e48ca75015ce5569f989
val <vale.dorval@gmail.com>
2020-10-14 17:50:24 -0400

    single page

--- single.php
+++ single.php
@@ -4,25 +4,40 @@ get_header();
 ?>
 
 <main class="main">
-
-     <?php while (have_posts()) : the_post(); ?>
-
-          <h3 class="heading-tertiary"><?php the_title(); ?></h3>
-          <p><?php the_author(); ?></p>
-          <p><?php the_date(); ?></p>
-          <?php
-
-          the_post_thumbnail();
-          the_content();
-
-          ?>
-
+     <nav class="blog__categorie flex u-margin-bottom-big">
 
           <?php
+          $categories = get_categories();
+          foreach ($categories as $categorie) { ?>
+          <?php echo '<div class="blog__categorie--item"><a href="' . get_category_link($categorie->term_id) . '">' . $categorie->name . '</a></div>';
+          } ?>
+     </nav>
 
-          ?>
 
-     <?php endwhile; ?>
+     <section class="single-page container">
+          <?php while (have_posts()) : the_post(); ?>
+
+               <h3 class="heading-tertiary"><?php the_title(); ?></h3>
+               <div class="single-page__info flex u-margin-top-xsmall">
+                    <p>Auteur: <?php the_author(); ?></p>
+                    <p>Date de publication: <?php the_date(); ?></p>
+               </div>
+               <div class="single-page__excerpt u-margin-top-big">
+                    <?php the_excerpt(); ?>
+               </div>
+               <div class="single-page__img center u-margin-top-normal u-margin-bottom-big">
+                    <?php the_post_thumbnail(); ?>
+
+               </div>
+
+               <div class="single-page__content">
+                    <?php the_content(); ?>
+               </div>
+
+     </section>
+
+
+<?php endwhile; ?>
 </main>
 
 
